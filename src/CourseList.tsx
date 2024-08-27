522c5df645de8e77d88892a9ad068b24604504a6
rushi-kesh-19 <111176841+rushi-kesh-19@users.noreply.github.com>
2024-08-28 03:20:12 +0530

    Update CourseList.tsx

--- src/CourseList.tsx
+++ src/CourseList.tsx
@@ -7,7 +7,6 @@ import { Link } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
-import * as React from 'react';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -26,12 +25,6 @@ const ImageWrapper = styled('div')({
 
 const TextWrapper = styled('div')({
   flex: '1 1 auto',
-});
-
-const Image = styled('img')({
-  width: '100%',
-  borderRadius: '8px',
-  objectFit: 'cover',
 });
 
 function CourseList() {
@@ -54,22 +47,38 @@ function CourseList() {
   )
 }
 
-export function Todos(props) {
+
+interface TodosProps {
+  title: string;
+  description: string;
+  price: number;
+  link: string;
+}
+
+export function Todos({ title, description, price, link }: TodosProps) {
   return (
     <div style={{ marginBottom: 20, padding: '1rem', borderRadius: 8, display: 'flex', alignItems: 'center' }}>
       <ImageWrapper>
-        <img src={props.link} alt={props.title} style={{ borderRadius: 8, width: '100%', height: 'auto', objectFit: 'cover' }} />
+        <img src={link} alt={title} style={{ borderRadius: 8, width: '100%', height: 'auto', objectFit: 'cover' }} />
       </ImageWrapper>
       <TextWrapper>
-        <Typography variant='h5' style={{ fontWeight: 700, fontFamily: 'helvetica', marginBottom: 10 }}>{props.title}</Typography>
-        <Typography variant='body1' style={{ marginBottom: 10 }}>{props.description}</Typography>
-        <Typography variant='body2'>Price: Rs {props.price}/-</Typography>
+        <Typography variant='h5' style={{ fontWeight: 700, fontFamily: 'helvetica', marginBottom: 10 }}>{title}</Typography>
+        <Typography variant='body1' style={{ marginBottom: 10 }}>{description}</Typography>
+        <Typography variant='body2'>Price: Rs {price}/-</Typography>
       </TextWrapper>
     </div>
   );
 }
 
-function TodosDiv({ courses }) {
+interface Course {
+  _id: string;
+  title: string;
+  description: string;
+  price: number;
+  imgageLink: string;
+}
+
+function TodosDiv({ courses }: { courses: Course[] }) {
   return (
     <div style={{ display: "flex", padding: 50, justifyContent: "space-around", flexWrap: "wrap" }}>
 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
