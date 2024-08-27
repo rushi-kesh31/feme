e3e5d5f68dec07c1b761385d734586c74734d483
rushi-kesh-19 <111176841+rushi-kesh-19@users.noreply.github.com>
2024-08-28 04:40:31 +0530

    Update Course.tsx

--- src/Course.tsx
+++ src/Course.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import { useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
