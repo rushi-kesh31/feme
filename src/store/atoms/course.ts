ad6fd5ccd62ae095e69f018832fe702eeaf2f6e4
rushi-kesh-19 <111176841+rushi-kesh-19@users.noreply.github.com>
2024-08-28 01:28:11 +0530

    Update course.ts

--- src/store/atoms/course.ts
+++ src/store/atoms/course.ts
@@ -1,9 +1,27 @@
-import {atom} from 'recoil';
+// src/store/atoms/course.ts
 
-export const courseState = atom({
-    key:'courseState',
-    default:{
-        isLoading:true,
-        course:null
-    }
-})
+import { atom } from 'recoil';
+
+interface Course {
+  title: string;
+  description: string;
+  price: number;
+  imageLink: string;
+  published: boolean;
+  _id: string;
+  productd: string;
+  rating: number;
+}
+
+interface CourseState {
+  isLoading: boolean;
+  course: Course | null; // Course can be null initially
+}
+
+export const courseState = atom<CourseState>({
+  key: 'courseState',
+  default: {
+    isLoading: true,
+    course: null,
+  },
+});
