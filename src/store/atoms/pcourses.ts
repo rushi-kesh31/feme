965765763f05c5b71822e4ada672439b3b7681b0
rushi-kesh-19 <111176841+rushi-kesh-19@users.noreply.github.com>
2024-08-28 01:41:52 +0530

    Update pcourses.ts

--- src/store/atoms/pcourses.ts
+++ src/store/atoms/pcourses.ts
@@ -1,6 +1,15 @@
-import {atom} from 'recoil';
+import { atom } from 'recoil';
 
-export const pcourseState = atom({
-    key:'pcourseState',
-    default:[]
-})
+
+interface PCourseState {
+  isLoading: boolean;
+  pcourse: []; // Assuming it's an array of courses
+}
+
+export const pcourseState = atom<PCourseState>({
+  key: 'pcourseState',
+  default: {
+    isLoading: true,
+    pcourse: [], // Default to an empty array
+  },
+});
