b12ecf0176a3f21f44019b6997ee0fd32d39d89e
rushi-kesh-19 <111176841+rushi-kesh-19@users.noreply.github.com>
2024-08-28 01:43:21 +0530

    Update pcSL.ts

--- src/store/selectors/pcSL.ts
+++ src/store/selectors/pcSL.ts
@@ -1,20 +1,18 @@
 import { selector } from "recoil";
 import { pcourseState } from "../atoms/pcourses";
 
-export const pcLoadingState= selector({
-    key:'pcLoadingState',
-    get:({get})=>{
-        const state= get(pcourseState);
-        return state.isLoading;
-        
-    }
-})
+export const pcLoadingState = selector({
+  key: 'pcLoadingState',
+  get: ({ get }) => {
+    const state = get(pcourseState);
+    return state.isLoading;
+  },
+});
 
-
-export const pcSelector= selector({
-    key:'pcSelector',
-    get:({get})=>{
-        const state= get(pcourseState);
-        return state.pcourse;
-    }
-})
+export const pcSelector = selector({
+  key: 'pcSelector',
+  get: ({ get }) => {
+    const state = get(pcourseState);
+    return state.pcourse; // Now accessing the `pcourse` array correctly
+  },
+});
