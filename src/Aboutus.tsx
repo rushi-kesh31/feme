94acf0f06e9421367eb98023b744888b65f972c5
rushi-kesh31 <nrushikesh219@gmail.com>
2024-08-28 14:33:56 +0530

    Create Aboutus.tsx

---
+++ src/Aboutus.tsx
@@ -0,0 +1,18 @@
+function Aboutus() {
+  return (
+    <>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+      }}>
+        Hustlers from NIT Rourkela
+      </div>
+    </>
+  );
+}
+
+export default Aboutus;
