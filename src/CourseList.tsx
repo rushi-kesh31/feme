ed5684a6af6e53491e2fd34b590f89dff4181d70
rushi-kesh31 <nrushikesh219@gmail.com>
2024-08-28 12:11:33 +0530

    Update CourseList.tsx

--- src/CourseList.tsx
+++ src/CourseList.tsx
@@ -8,6 +8,7 @@ import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
+import Footer from './Footer';
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -98,6 +99,7 @@ function TodosDiv({ courses }: { courses: Course[] }) {
           );
         })}
       </Grid>
+      <Footer />
     </div>
   );
 }
